12fcdd1541977f1b65b14aee796a2a1218c70194
akintadetemi19@gmail.com <akintadetemi19@gmail.com>
2023-07-02 23:56:23 +0100

    started regular expression

--- password.js
+++ password.js
@@ -1,11 +1,39 @@
 let form = document.querySelector('form')
 
-let matches = {
-	uppercase: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-	lowercase: (x) => {
-		return matches.uppercase.map((val, ind, arr) => (
-				val.toLowerCase()
-		))
+let classXchange = (el, initial, final) => {
+	document.querySelector(el).classList.remove(initial)
+	document.querySelector(el).classList.add(final)
+}
+
+let checkPassword = (str) => {
+	if (str.match('.{8,}')) {
+		classXchange('#length', 'unvalidated', 'validated')
+	} else {
+		classXchange('#length', 'validated', 'unvalidated')
+	}
+	
+	if (str.match('[a-z]')) {
+		classXchange('#lowercase','unvalidated','validated')
+	} else {
+		classXchange('#lowercase','validated','unvalidated')
+	}
+	
+	if (str.match('[A-Z]')) {
+		classXchange('#uppercase','unvalidated','validated')
+	} else {
+		classXchange('#uppercase','validated','unvalidated')
+	}
+	
+	if (str.match('[0-9]')) {
+		classXchange('#number', 'unvalidated', 'validated')
+	} else {
+		classXchange('#number', 'validated', 'unvalidated')
+	}
+	
+	if (str.match('\i','[a-zA-Z0-9]')) {
+		classXchange('#special', 'validated', 'unvalidated')
+	} else {
+		classXchange('#special', 'unvalidated', 'validated')
 	}
 }
 
@@ -13,11 +41,12 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault()
 	
 	let userPassword = password.value.toString()
-	
+	if (userPassword.length === 0 || userPassword.value === '') {
+		alert('Password cannot be empty!')
+		document.querySelectorAll('#conditions ul li').forEach((val, ind, arr) => {
+			val.classList.add('unvalidated')
+		})
+	}else{
+		checkPassword(userPassword)
+	}
 })
-
-x = matches.uppercase.map((val, ind, arr) => {
-	return val.toLowerCase()
-})
-
-alert(matches.lowercase())
